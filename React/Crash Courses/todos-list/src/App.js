f9c31db97970c31f9176989755dd8d0075ca675a
Shoaib4447 <125224624+Shoaib4447@users.noreply.github.com>
2023-04-29 09:47:07 +0500

    Just commit

--- React/Crash Courses/todos-list/src/App.js
+++ React/Crash Courses/todos-list/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./MyComponents/Header";
+import Footer from "./MyComponents/Footer";
+import Todoitem from "./MyComponents/Todoitem";
+import Todos from "./MyComponents/Todos";
+
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title='ToDos List' searchBar={true}/>
+      <Todos></Todos>
+      <Todoitem/>
+      <Footer/>
+    </>
   );
 }
 
 export default App;
+
+
