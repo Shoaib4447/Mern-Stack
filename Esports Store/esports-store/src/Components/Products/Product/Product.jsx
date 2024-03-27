f8a2462027c58eac13566624c84ea02810dd8c6c
Shoaib4447 <125224624+Shoaib4447@users.noreply.github.com>
2024-03-27 21:26:43 +0500

    Update Product.jsx

--- Esports Store/esports-store/src/Components/Products/Product/Product.jsx
+++ Esports Store/esports-store/src/Components/Products/Product/Product.jsx
@@ -1,45 +1,45 @@
-import React from 'react';
-import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@mui/core';
-import { AddShoppingCart } from '@mui/icons-material';
+// import React from 'react';
+// import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@mui/core';
+// import { AddShoppingCart } from '@mui/icons-material';
 
-import useStyles from './Styles';
+// import useStyles from './Styles';
 
-const Product = ({ product, onAddToCart }) => {
-  const classes = useStyles();
+// const Product = ({ product, onAddToCart }) => {
+//   const classes = useStyles();
 
-  const handleAddToCart = () => onAddToCart(product.id, 1);
+//   const handleAddToCart = () => onAddToCart(product.id, 1);
 
-  return (
-    // <Card className={classes.root}>
-    //   <CardMedia className={classes.media} image={product.media.source} title={product.name} />
-    //   <CardContent>
-    //     <div className={classes.cardContent}>
-    //       <Typography gutterBottom variant="h5" component="h2">
-    //         {product.name}
-    //       </Typography>
-    //       <Typography gutterBottom variant="h5" component="h2">
-    //         ${product.price.formatted}
-    //       </Typography>
-    //     </div>
-    //     <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" component="p" />
-    //   </CardContent>
-    //   <CardActions disableSpacing className={classes.cardActions}>
-    //     <IconButton aria-label="Add to Cart" onClick={handleAddToCart}>
-    //       <AddShoppingCart />
-    //     </IconButton>
-    //   </CardActions>
-    // </Card>
+//   return (
+//     // <Card className={classes.root}>
+//     //   <CardMedia className={classes.media} image={product.media.source} title={product.name} />
+//     //   <CardContent>
+//     //     <div className={classes.cardContent}>
+//     //       <Typography gutterBottom variant="h5" component="h2">
+//     //         {product.name}
+//     //       </Typography>
+//     //       <Typography gutterBottom variant="h5" component="h2">
+//     //         ${product.price.formatted}
+//     //       </Typography>
+//     //     </div>
+//     //     <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" component="p" />
+//     //   </CardContent>
+//     //   <CardActions disableSpacing className={classes.cardActions}>
+//     //     <IconButton aria-label="Add to Cart" onClick={handleAddToCart}>
+//     //       <AddShoppingCart />
+//     //     </IconButton>
+//     //   </CardActions>
+//     // </Card>
 
-    <div className="card" style={{ width: '18rem' }}>
-      <img src="..." className="card-img-top" alt="Product image" />
-      <div className="card-body d-flex">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">$5</p>
-      </div>
-      <p className='card-text'></p>
-    </div>
+//     <div className="card" style={{ width: '18rem' }}>
+//       <img src="..." className="card-img-top" alt="Product image" />
+//       <div className="card-body d-flex">
+//         <h5 className="card-title">Card title</h5>
+//         <p className="card-text">$5</p>
+//       </div>
+//       <p className='card-text'></p>
+//     </div>
 
-  );
-};
+//   );
+// };
 
-export default Product;
+// export default Product;
